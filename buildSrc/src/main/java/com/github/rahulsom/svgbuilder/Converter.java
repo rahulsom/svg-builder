1723a440f0efda026da868025e71cf67fe794e35
Rahul Somasunderam <rahul.som@gmail.com>
2022-09-18 14:49:05 -0700

    chore: Upgrade to JDK 17

--- buildSrc/src/main/java/com/github/rahulsom/svgbuilder/Converter.java
+++ buildSrc/src/main/java/com/github/rahulsom/svgbuilder/Converter.java
@@ -2,13 +2,15 @@ package com.github.rahulsom.svgbuilder;
 
 import com.thaiopensource.relaxng.translate.Driver;
 
+import java.io.File;
+
 public class Converter {
-    public void convert() {
+    public void convert(File buildDir) {
         new Driver().run(new String[]{
                 "-I", "dtd",
                 "-O", "xsd",
-                "build/schemas/svg.dtd",
-                "build/schemas/svg.xsd"
+                new File(buildDir, "schemas/svg.dtd").getAbsolutePath(),
+                new File(buildDir,"schemas/svg.xsd").getAbsolutePath()
         });
     }
 }
--- buildSrc/src/main/java/com/github/rahulsom/svgbuilder/GroovyNewifyBuilder.java
+++ buildSrc/src/main/java/com/github/rahulsom/svgbuilder/GroovyNewifyBuilder.java
@@ -8,20 +8,20 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class GroovyNewifyBuilder {
-    public void createFile() {
-        String sourcesDir = "build/generated/sources/xjc/java/main/com/github/rahulsom/svg";
+    public void createFile(File buildDir) {
+        String sourcesDir = new File(buildDir, "generated/sources/xjc/java/main/com/github/rahulsom/svg").getAbsolutePath();
         String classNames = Arrays.stream(new File(sourcesDir).listFiles())
                 .map(it -> it.getName().replace(".java", ""))
                 .filter(it -> !it.equals("package-info"))
                 .collect(Collectors.joining(", ", "@Newify([", "])"));
 
         try {
-            File dir = new File("build/groovysupport/generated-sources/com/github/rahulsom/svg/");
+            File dir = new File(buildDir, "groovysupport/generated-sources/com/github/rahulsom/svg/");
             if (!dir.exists()) {
                 dir.mkdirs();
             }
             PrintWriter fileWriter =
-                    new PrintWriter(new FileWriter(new File( "build/groovysupport/generated-sources/com/github/rahulsom/svg/NewifySvg.groovy")));
+                    new PrintWriter(new FileWriter(new File(buildDir, "groovysupport/generated-sources/com/github/rahulsom/svg/NewifySvg.groovy")));
             fileWriter.println("package com.github.rahulsom.svg");
             fileWriter.println("");
             fileWriter.println("import groovy.transform.AnnotationCollector");
