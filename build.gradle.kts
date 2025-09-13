8646d59df1b991a4e9524c6bb30c7055704778ae
Rahul Somasunderam <rahul.som@gmail.com>
2025-09-12 18:40:41 -0700

    chore: Create version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import com.github.rahulsom.waena.WaenaExtension
 
 plugins {
-    id("com.github.rahulsom.svg-builder")
+    alias(libs.plugins.svg.builder)
     id("java")
     id("groovy")
-    id("org.jetbrains.kotlin.jvm") version "2.2.20"
-    id("com.github.rahulsom.waena.root") version "0.18.0"
-    id("com.github.rahulsom.waena.published") version "0.18.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.waena.root)
+    alias(libs.plugins.waena.published)
 }
 
 contacts {
@@ -35,23 +35,16 @@ sourceSets {
 val xjcTool by configurations.creating
 
 dependencies {
-    xjcTool("org.glassfish.jaxb:jaxb-xjc:4.0.5")
-    xjcTool("org.glassfish.jaxb:jaxb-runtime:4.0.5")
-    xjcTool("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
-    xjcTool("javax.xml.bind:jaxb-api:2.3.1")
-    xjcTool("org.jvnet.jaxb2_commons:jaxb2-fluent-api:3.0")
-    xjcTool("org.jvnet.jaxb2_commons:jaxb2-basics-annotate:1.1.0")
+    xjcTool(libs.bundles.xjc.tools)
 
-    compileOnly("org.codehaus.groovy:groovy-all:3.0.25")
+    compileOnly(libs.groovy.all)
 
-    implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
-    implementation("org.glassfish.jaxb:jaxb-runtime:4.0.5")
-    implementation("com.sun.xml.bind:jaxb-core:4.0.5")
+    implementation(libs.bundles.jaxb.runtime)
 
-    testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation(libs.spock.core)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    testImplementation(libs.kotlin.stdlib.jdk8)
+    compileOnly(libs.kotlin.stdlib.jdk8)
 }
 
 tasks.downloadSvgDtd.configure {
